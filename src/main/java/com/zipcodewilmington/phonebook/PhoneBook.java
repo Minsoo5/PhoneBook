e834335f334fe022bbe94ae9c7985b4ac8f6fae6
min <minsoojo5@gmail.com>
2023-07-12 18:19:21 -0400

    remove test done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -21,25 +21,46 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        if (phoneBook.containsKey(name)) {
+            phoneBook.get(name).add(phoneNumber);
+        } else {
+            phoneBook.put(name, new ArrayList<>());
+            phoneBook.get(name).add(phoneNumber);
+        }
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for (String s : phoneNumbers) {
+            add(name, s);
+        }
     }
 
     public void remove(String name) {
+        if (hasEntry(name)) {
+            phoneBook.remove(name);
+        }
     }
 
     public Boolean hasEntry(String name, String phoneNumber) {
-        return null;
+        if (hasEntry(name) && phoneBook.get(name).contains(phoneNumber)) {
+            return true;
+        }
+        return false;
     }
 
-    public Boolean hasEntry(String name) {return null;}
+    public Boolean hasEntry(String name) {
+        return phoneBook.containsKey(name);
+    }
 
     public List<String> lookup(String name) {
-        return null;
+        return phoneBook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+
+
+
         return null;
     }
 
